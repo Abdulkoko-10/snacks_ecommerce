8f6d6e138af09f1acedd1760b5c44729f7a763a8
Abdulkoko-10 <110394302+Abdulkoko-10@users.noreply.github.com>
2025-05-20 15:31:23 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
-import { BsSun, BsMoon } from 'react-icons/bs' // Importing theme icons
 
 import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities, theme, toggleTheme } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
     <div className="navbar-container">
@@ -15,22 +14,11 @@ const Navbar = () => {
         <Link href="/">Snacks</Link>
       </p>
 
-      <div style={{ display: 'flex', alignItems: 'center' }}> {/* Wrapper for cart and theme toggle */}
-        <button 
-          type="button"
-          className="cart-icon" 
-          onClick={toggleTheme} 
-          style={{ marginRight: '20px', fontSize: '20px' }} // Added styling for the theme toggle
-        >
-          {theme === 'light' ? <BsMoon /> : <BsSun />}
-        </button>
-
-        <button type="button"
-        className="cart-icon" onClick={() => setShowCart(true)}>
-          <AiOutlineShopping /> {/* It seems there's an extra 's' here, remove if not intended */}
-          <span className="cart-item-qty">{totalQuantities}</span>
-        </button>
-      </div>
+      <button type="button"
+      className="cart-icon" onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />s
+        <span className="cart-item-qty">{totalQuantities}</span>
+      </button>
 
 {/* This code snippet is using JavaScript's template literal syntax
  and is rendering a Cart component if the showCart variable is true. */}
